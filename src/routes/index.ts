ab4d1c9d334690872bccc5a8ea9f9804711c4265
Derick <d.a.s2000silva14@gmail.com>
2022-09-09 13:44:29 -0300

    fix/correção de bug do docker

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,16 @@ import schoolRoutes from "./school.routes";
 import sessionRoutes from "./session.routes";
 import teamsRoutes from "./team.routes";
 import teacherRoutes from "./teachers.routes";
-import studentRoutes from './students.routes';
+import studentRoutes from "./students.routes";
+import feedbackRoutes from "./feedback.routes";
 
-const AppRoutes = (app:Express) => {
+const AppRoutes = (app: Express) => {
   app.use("/schools", schoolRoutes());
   app.use("/login", sessionRoutes());
   app.use("/teams", teamsRoutes());
   app.use("/teachers", teacherRoutes());
-  app.use('/students',studentRoutes());
+  app.use("/students", studentRoutes());
+  app.use("/feedback", feedbackRoutes());
 };
 
 export default AppRoutes;
