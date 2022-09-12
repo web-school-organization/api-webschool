0bdb8eebfa3f22b519766a6703d1a1c69e023728
LucasDeleonPaz <lucasdeleonpaz@gmail.com>
2022-09-12 15:43:21 -0300

    feat/Criação dos testes para rota Activities - Criação da estrutura de pastas gerais para a rota Activities

---
+++ src/services/activities/updateActivities.service.ts
@@ -0,0 +1,9 @@
+import AppDataSource from "../../data-source";
+import { Activities } from "../../entities/activities.entity";
+import { AppError } from "../../errors/app.error";
+
+const updateActivitiesService = async (id: string) => {
+
+}
+
+export default updateActivitiesService
