182506796519ab63201a685acb3b7a2ad2a21d62
Luccas Queiroz <luccasqueiroz7@hotmail.com>
2022-09-14 15:10:51 -0300

    fix/ removendo os passwords das rotas

--- src/controllers/activities/activities.controller.ts
+++ src/controllers/activities/activities.controller.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import createActivitiesService from "../../services/activities/createActivities.service";
 import deleteActivitiesService from "../../services/activities/deleteActivities.service";
@@ -5,26 +6,31 @@ import getActivitiesService from "../../services/activities/getActivities.servic
 import updateActivitiesService from "../../services/activities/updateActivities.service";
 
 const createActivitiesController = async (req: Request, res: Response) => {
-    const newActivitie = await createActivitiesService(req.body, req.user.type)
-    return res.status(201).json(newActivitie)
-}
+  const newActivitie = await createActivitiesService(req.body, req.user.type);
+  return res.status(201).json(instanceToPlain(newActivitie));
+};
 
-const deleteActivitiesController = async ( req: Request, res: Response ) => {
-    const { id } = req.params
-    const deletedActivitie = await deleteActivitiesService(id)
-    return res.status(204).json(deletedActivitie)
-}
+const deleteActivitiesController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deletedActivitie = await deleteActivitiesService(id);
+  return res.status(204).json(deletedActivitie);
+};
 
 const getActivitiesController = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const listActivities = await getActivitiesService(id)
-    return res.status(200).json(listActivities)
-}
+  const { id } = req.params;
+  const listActivities = await getActivitiesService(id);
+  return res.status(200).json(instanceToPlain(listActivities));
+};
 
 const updateActivitiesController = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const updatedActivitie = await updateActivitiesService(req.body, id)
-    return res.status(200).json(updatedActivitie)
-}
+  const { id } = req.params;
+  const updatedActivitie = await updateActivitiesService(req.body, id);
+  return res.status(200).json(instanceToPlain(updatedActivitie));
+};
 
-export { createActivitiesController, deleteActivitiesController, getActivitiesController, updateActivitiesController }
+export {
+  createActivitiesController,
+  deleteActivitiesController,
+  getActivitiesController,
+  updateActivitiesController,
+};
