65475c4fef24bd94b33aa7c3bdc0a366baa86eda
Luccas Queiroz <luccasqueiroz7@hotmail.com>
2022-09-14 11:07:19 -0300

    fix/ Corrigindo erros do insomnia

--- src/services/responsibles/responsibleUpdate.service.ts
+++ src/services/responsibles/responsibleUpdate.service.ts
@@ -1,40 +1,38 @@
-import AppDataSource from "../../data-source"
-import { Responsibles } from "../../entities/responsible.entity"
-import { AppError } from "../../errors/app.error"
-import { IResponsibleRequest } from "../../interfaces/responsibles"
-import bcrypt from 'bcryptjs'
+import AppDataSource from "../../data-source";
+import { Responsibles } from "../../entities/responsible.entity";
+import { AppError } from "../../errors/app.error";
+import { IResponsibleRequest } from "../../interfaces/responsibles";
+import bcrypt from "bcryptjs";
 
+const responsibleUpdateService = async (
+  { name, email, password }: IResponsibleRequest,
+  id: string
+) => {
+  const responsiblesRepository = AppDataSource.getRepository(Responsibles);
 
-const responsibleUpdateService = async ({name,email,password}:IResponsibleRequest,id:string) => {
+  const responsible = await responsiblesRepository.findOneBy({ id });
 
-    const responsiblesRepository = AppDataSource.getRepository(Responsibles);
+  const emailExists = await responsiblesRepository.findOneBy({ email });
 
-    const responsible = await responsiblesRepository.findOneBy({id})
+  if (!responsible) {
+    throw new AppError("responsible not found", 404);
+  }
 
+  if (email !== responsible.email && emailExists) {
+    throw new AppError("email updated already exists", 400);
+  }
 
-    const emailExists = await responsiblesRepository.findOneBy({ email });
+  const updatedResponsible = {
+    name: name || responsible.name,
+    email: email || responsible.email,
+    password: bcrypt.hashSync(password, 10) || responsible.password,
+  };
 
-    
-    if(!responsible){
-        throw new AppError('responsible not found',404);
-    }
+  await responsiblesRepository.update(id, updatedResponsible);
 
-    if(emailExists){
-        throw new AppError('email updated already exists',400);
-    }
+  const responsibleUpdated = await responsiblesRepository.findOneBy({ id });
 
-    const updatedResponsible = {
-        name: name || responsible.name,
-        email: email || responsible.email,
-        password: bcrypt.hashSync(password,10) || responsible.password
+  return responsibleUpdated;
+};
 
-    }  
-
-    await responsiblesRepository.update(id,updatedResponsible);
-
-    const responsibleUpdated = await responsiblesRepository.findOneBy({id});
-
-    return responsibleUpdated;
-}
-
-export default responsibleUpdateService
+export default responsibleUpdateService;
